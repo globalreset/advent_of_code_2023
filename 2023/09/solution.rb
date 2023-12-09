5c23b323a4db405e15e405377f8fdd7f55162b61
globalreset <shughes@pobox.com>
2023-12-08 23:48:12 -0600

    adding day 9

---
+++ 2023/09/solution.rb
@@ -0,0 +1,21 @@
+module Year2023
+  class Day09 < Solution
+
+    def getNext(arr)
+      if(arr.all?{|a| a==0})
+         return 0
+      else
+        return arr.last + getNext(arr.each_cons(2).to_a.map{ |a,b| b-a })
+      end
+    end
+
+    def part_1
+      data.map(&:split).map { |a| a.map(&:to_i) }.map {|a| getNext(a) }.sum
+    end
+
+    def part_2
+      data.map(&:split).map { |a| a.map(&:to_i) }.map {|a| getNext(a.reverse) }.sum
+    end
+
+  end
+end
