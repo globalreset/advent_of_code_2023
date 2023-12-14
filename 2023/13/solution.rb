eee08a5d206a4fa2bf31ac7dc730d1a364594bf6
globalreset <shughes@pobox.com>
2023-12-13 22:14:21 -0600

    cleanup for legibility

--- 2023/13/solution.rb
+++ 2023/13/solution.rb
@@ -2,40 +2,29 @@
 module Year2023
   class Day13 < Solution
 
+    # allowed is number of mismatched locations in the reflection
     def find_reflection(tile, allowed=0)
       tile = tile.map { _1.join.gsub("#", "1").gsub(".", "0").to_i(2) }
-      (0..(tile.size-2)).find { |h|
-        space_on_right = [h, tile.size - h - 2].min + 1
-        space_on_left = [h, space_on_right-1].min
-        left = (h-space_on_left)..(h)
-        right = (h+1)..(h+space_on_right)
+      (1..(tile.size-1)).find { |h|
+        cnt = [h, tile.size - h].min
+        left = (h-cnt)..(h-1)
+        right = (h)..(h+cnt-1)
         tile[left].zip(tile[right].reverse).sum { Util.hamming_distance(_1,_2) } == allowed
       }
     end
 
     def part_1
-      data.slice_when { _1.size==0 }
-          .map { |tile| tile.reject(&:empty?).map(&:chars) }
-          .sum { |tile|
-        h_idx = find_reflection(tile)
-        if(h_idx==nil)
-          find_reflection(tile.transpose) + 1
-        else
-          (h_idx+1)*100
-        end
+      data.slice_when { _1.size==0 }.sum { |tile|
+        # need chars for transpose to work
+        tile = tile.reject(&:empty?).map(&:chars)
+        find_reflection(tile.transpose) || 100*find_reflection(tile)
       }
     end
 
     def part_2
-      data.slice_when { _1.size==0 }
-          .map { |tile| tile.reject(&:empty?).map(&:chars) }
-          .sum { |tile|
-        h_idx = find_reflection(tile, 1)
-        if(h_idx==nil)
-          find_reflection(tile.transpose, 1) + 1
-        else
-          (h_idx+1)*100
-        end
+      data.slice_when { _1.size==0 }.sum { |tile|
+        tile = tile.reject(&:empty?).map(&:chars)
+        find_reflection(tile.transpose, 1) || 100*find_reflection(tile, 1)
       }
     end
 
